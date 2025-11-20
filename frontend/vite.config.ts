ff10980e07c4e47be8a6d01afdfc27f1d3aaa59e
Shivdutt Karwa <shivduttkarwa@gmail.com>
2025-11-20 20:06:54 +0530

    fonts fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,7 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/shambala_homes_react_headless/",
+  base: process.env.NODE_ENV === 'production' ? '/shambala_homes_react_headless/' : '/',
   plugins: [react()],
   resolve: {
     alias: {
