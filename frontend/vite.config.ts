ab06cc92d06ceb1552c428a51753d7a7ceb3a70c
Shivdutt Karwa <shivduttkarwa@gmail.com>
2025-11-20 20:23:01 +0530

    hero animation fix

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,7 +4,10 @@ import { fileURLToPath, URL } from "node:url";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/shambala_homes_react_headless/' : '/',
+  base:
+    process.env.NODE_ENV === "production"
+      ? "/shambala_homes_react_headless/"
+      : "/",
   plugins: [react()],
   resolve: {
     alias: {
